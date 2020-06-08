09c8b60bedf34c74e9050f870b0494ca28376870
AgnesNi <49183919+AgnesNi@users.noreply.github.com>
2020-06-08 11:16:13 +0800

    Update fatTree.py

--- fatTree.py
+++ fatTree.py
@@ -3,7 +3,7 @@
 from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController
 from mininet.node import CPULimitedHost, Host, Node
-from mininet.node import OVSKernetSwitch, UserSwitch
+from mininet.node import OVSKernelSwitch, UserSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
